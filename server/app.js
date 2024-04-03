a07702b79dc3406e55a4b778f1b9585eb88e8a16
IfeanyiMiracleIfeanyi <ifeanyimiraclechuks01@gmail.com>
2024-04-03 18:01:25 +0100

    finally

--- server/app.js
+++ server/app.js
@@ -51,10 +51,10 @@ app.get("/login/failed", (req, res) => {
 app.get("/login/success", (req, res) => {
   if (req.isAuthenticated()) {
   }
-  const { password, ...rest } = req.user;
+  // const { password, ...rest } = req.user;
   return res
     .status(200)
-    .json({ msg: "Login Successful!", route: "/", user: { ...rest } });
+    .json({ msg: "Login Successful!", route: "/", user: req.user });
   return res.status(200).json({
     msg: "Login Failed! try again!",
     route: "/login",
